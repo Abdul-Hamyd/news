f20fd486d91923c1915a10af550b9597d1e17400
Abdul-Hamyd <102166699+Abdul-Hamyd@users.noreply.github.com>
2023-10-01 17:39:51 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const but = document.querySelector("#button");
+but.addEventListener("click", () => {
+    but.style.backgroundColor = "black";
+    alert("Sorry, the Read More feature is not available now.")
+})
+
